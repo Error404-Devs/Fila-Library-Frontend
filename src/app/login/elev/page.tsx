1d3a429afff8c9c322342b05a36ee7aba6bbd3a4
David Rotariu <davidrotariu139@gmail.com>
2024-05-10 18:22:30 +0300

    Installed prettier

--- src/app/login/elev/page.tsx
+++ src/app/login/elev/page.tsx
@@ -1,11 +1,11 @@
-import { LoginForm } from "./LoginForm"
+import { LoginForm } from './LoginForm';
 
 const LoginElev = () => {
-  return (
-    <div className="flex justify-center items-center h-[90vh]">
-        <LoginForm />
-    </div>
-  )
-}
+    return (
+        <div className="flex justify-center items-center h-[90vh]">
+            <LoginForm />
+        </div>
+    );
+};
 
-export default LoginElev
+export default LoginElev;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-[90vh]">
-        <Link href="/login/elev">
-            <h1 className="text-7xl font-bold tracking-widesr">Fila Library</h1>
-        </Link>
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center h-[90vh]">
+            <Link href="/login/elev">
+                <h1 className="text-7xl font-bold tracking-widesr">
+                    Fila Library
+                </h1>
+            </Link>
+        </div>
+    );
 }
