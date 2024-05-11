b33bdd853e954fbd0b393f7e58f737e6025f8355
David Rotariu <davidrotariu139@gmail.com>
2024-05-11 20:57:49 +0300

    Implemented epic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,47 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-[90vh]">
-            <Link href="/login/elev">
-                <h1 className="text-7xl font-bold tracking-widesr">
-                    Fila Library
-                </h1>
-            </Link>
-        </div>
+        <>
+            <div className="relative flex flex-col items-center w-full h-screen">
+                <Image
+                    src="/backgroundtop.svg"
+                    alt="Top cover"
+                    style={{
+                        width: '100%',
+                        height: '40%',
+                        objectFit: 'cover'
+                    }}
+                    width={960}
+                    height={300}
+                />
+
+                <div className="h-[20vh] flex justify-center items-center">
+                    <Image
+                        src="/filalogo.png"
+                        width={200}
+                        height={200}
+                        alt="Fila Logo"
+                        className="pb-[20px]"
+                    />
+                    <Link href="/login/elev">
+                        <h1 className="text-7xl font-bold tracking-wider text-[#2b3167] cursor-pointer">
+                            Fila Library
+                        </h1>
+                    </Link>
+                </div>
+
+                <Image
+                    src="/backgroundbottom.svg"
+                    alt="Bottom cover"
+                    style={{
+                        width: '100%',
+                        height: '40%',
+                        objectFit: 'cover'
+                    }}
+                    width={960}
+                    height={300}
+                />
+            </div>
+        </>
     );
 }
