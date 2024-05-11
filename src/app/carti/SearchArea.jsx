fa41c27dc46e3e9a0588e29e12918eec2605d1bd
David Rotariu <davidrotariu139@gmail.com>
2024-05-11 13:18:32 +0300

    Added search button (its harder than it looks)

--- src/app/carti/SearchArea.jsx
+++ src/app/carti/SearchArea.jsx
@@ -1,65 +1,70 @@
 import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const SearchArea = () => {
     return (
-        <div className="w-full flex-1">
-            <form>
-                <div className="flex gap-2 py-2">
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search books..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
+        <>
+            <div className="w-full flex-1">
+                <form>
+                    <div className="flex gap-2 py-2">
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search books..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search autori..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
                     </div>
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search autori..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
+                    <div className="flex gap-2 py-2">
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search cota..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search editura..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search an..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
+                        <div className="flex-1 relative">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search loc..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="flex gap-2 py-2">
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search cota..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
-                    </div>
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search editura..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
-                    </div>
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search an..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
-                    </div>
-                    <div className="flex-1 relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search loc..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none"
-                        />
-                    </div>
-                </div>
-            </form>
-
-        </div>
+                </form>
+            </div>
+            <Button variant="outline" className="h-[48px] w-[48px] p-4">
+                <Search className="h-8 w-8" />
+            </Button>
+        </>
     );
 };
 
