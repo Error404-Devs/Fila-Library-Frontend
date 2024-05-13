da2fb523435939a61e59f331606d1a8d1799cc02
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-05-13 20:44:32 +0300

    Implementing book cards and layout

--- src/app/situatie/page.tsx
+++ src/app/situatie/page.tsx
@@ -1,10 +1,27 @@
-import { SituationTable } from "./SituationTable";
+import { StudentStatus } from "./studentStatus";
+import { Library } from 'lucide-react';
+import SearchBar from "./SearchArea";
+import Link from 'next/link';
 
-const Dashboard = async () => {
+function Dashboard() {
 
     return (
         <>
-            <SituationTable/>
+            <div className="flex flex-col">
+                <header className="flex h-6 items-center gap-4 border-b bg-muted/40 px-4 md:h-[6rem] lg:h-[6rem] lg:px-6">
+                    <Link
+                        href="/"
+                        className="flex items-center gap-2 font-semibold"
+                    >
+                        <Library className="h-6 w-6" />
+                        <span className="text-xl">Filadelfia</span>
+                    </Link>
+                    <div className="px-16 w-full">
+                        <SearchBar />
+                    </div>
+                </header>
+            </div>
+            <StudentStatus/>
         </>
     );
 };
