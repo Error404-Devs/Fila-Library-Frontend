3bd56735aa3945e58accd1439844bd117c8494ec
David Rotariu <davidrotariu139@gmail.com>
2024-05-20 13:28:24 +0300

    Started implementing borrow feature

--- src/app/carti/ImprumutaModal.tsx
+++ src/app/carti/ImprumutaModal.tsx
@@ -8,6 +8,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 
 interface ImprumutaModalProps {
     bookName: string;
@@ -15,33 +16,80 @@ interface ImprumutaModalProps {
 
 const ImprumutaModal = ({ bookName }: ImprumutaModalProps) => {
     return (
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="max-w-[90vh]">
             <DialogHeader>
                 <DialogTitle>Imprumuta Cartea: {bookName}</DialogTitle>
                 <DialogDescription>
                     Completetati datele elevelui care imprumuta cartea
                 </DialogDescription>
             </DialogHeader>
-            <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                        Nume
-                    </Label>
-                    <Input
-                        id="username"
-                        defaultValue="Rotariu"
-                        className="col-span-3"
-                    />
+            <div className="flex gap-4 py-4">
+                <div>
+                    <h4 className="mb-6 text-xl font-semibold tracking-tight">
+                        Date despre{' '}
+                        <span className="underline underline-offset-4">
+                            carte
+                        </span>
+                    </h4>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Titlu
+                        </Label>
+                        <Input
+                            id="username"
+                            defaultValue={bookName}
+                            className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Autor
+                        </Label>
+                        <Input
+                            id="name"
+                            defaultValue="David"
+                            className="col-span-3"
+                        />
+                    </div>
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                        Prenume
-                    </Label>
-                    <Input
-                        id="name"
-                        defaultValue="David"
-                        className="col-span-3"
-                    />
+                <Separator orientation="vertical" />
+                <div>
+                    <h4 className="mb-6 text-xl font-semibold tracking-tight">
+                        Date despre{' '}
+                        <span className="underline underline-offset-4">
+                            elev
+                        </span>
+                    </h4>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Nr Matricol
+                        </Label>
+                        <Input
+                            id="username"
+                            defaultValue="Rotariu"
+                            className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Nume
+                        </Label>
+                        <Input
+                            id="username"
+                            defaultValue="Rotariu"
+                            className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Prenume
+                        </Label>
+                        <Input
+                            id="name"
+                            defaultValue="David"
+                            className="col-span-3"
+                        />
+                    </div>
                 </div>
             </div>
             <DialogFooter>
