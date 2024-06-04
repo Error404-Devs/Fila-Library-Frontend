6e283c2a80d926c8bc1c4aa8eb184735ab12b5f1
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-06-04 16:02:48 +0300

    Refactoring sing up

--- src/app/login/elev/LoginForm.tsx
+++ src/app/login/elev/LoginForm.tsx
@@ -1,4 +1,6 @@
 'use client'
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
 import { Button } from '@/components/ui/button';
 import {
     Card,
@@ -14,78 +16,128 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
+import { useTheme } from 'next-themes';
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
 
 export function LoginForm() {
 
-    const [nr_crt, setNrCrt] = useState('')
+    const [name, setName] = useState('')
+    const [prenume, setPrenume] = useState('')
+
     const router = useRouter();
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const handleNrCrtChange = (e:any) => {
-        setNrCrt(e.target.value);
-        console.log(nr_crt)
+    const { setTheme } = useTheme()
+
+    const handleNameChange = (e:any) => {
+        setName(e.target.value);
+        console.log(name)
+    };
+    const handlePrenumeChange = (e:any) => {
+        setPrenume(e.target.value);
+        console.log(prenume)
     };
 
     const handleSubmit = async (e: any) => {
         e.preventDefault(); 
         setLoading(true);
     try {
-        const response = await fetch(`${baseUrl}/borrows?person_id=${nr_crt}`);
+        const response = await fetch(`${baseUrl}/borrows?first_name=${name}&last_name=${prenume}`);
         if (!response.ok) {
-            throw new Error('Nr. Matricol not found');
+            throw new Error('Utilizator not found');
         }
-        router.push(`/situatie?nr_crt=${nr_crt}`);
+        router.push(`/situatie?nr_crt=${name}`);
     } catch (error) {
-        setError("Nr. Matricol invalid");
+        setError("Utilizator invalid");
         setLoading(false);
     };
 }
-
+    
     return (
-        <Card className="w-full max-w-sm">
-            <CardHeader>
-                <CardTitle className="text-2xl">Login Elev</CardTitle>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-                <form onSubmit={handleSubmit} className="grid gap-4">
-                    <div className="grid gap-2">
-                        <Label htmlFor="nrmatricol">Nr. Matricol</Label>
-                        <Input
-                            id="nrmatricol"
-                            type="text"
-                            placeholder="39 VIII L"
-                            required
-                            value={nr_crt}
-                            onChange={handleNrCrtChange}
-                        />
-                    </div>
-                    <CardFooter>
-                        <div className='flex flex-col w-full'>
-                            {error && (
-                                <div className="text-red-500 text-sm text-center my-2">
-                                    {error}
-                                </div>
-                            )}
-                            <Button
-                                type="submit"
-                                className="w-full"
-                                disabled={loading}
-                            >
-                                {loading ? (
-                                    <>
-                                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                                        Please wait
-                                    </>
-                                ) : (
-                                    'Sign in'
-                                )}
+        <>
+                <div style={{ position: 'absolute', top: 0, right: 0, margin: '10px' }}>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon">
+                            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                            <span className="sr-only">Toggle theme</span>
                             </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login Elev</CardTitle>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <form onSubmit={handleSubmit} className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="nrmatricol">Nume</Label>
+                            <Input
+                                id="nrmatricol"
+                                type="text"
+                                placeholder="Nume"
+                                required
+                                value={name}
+                                onChange={handleNameChange}
+                            />
                         </div>
-                    </CardFooter>
-                </form>
-            </CardContent>
-        </Card>
+                        <div className="grid gap-2">
+                            <Label htmlFor="nrmatricol">Prenume</Label>
+                            <Input
+                                id="nrmatricol"
+                                type="text"
+                                placeholder="Prenume"
+                                required
+                                value={prenume}
+                                onChange={handlePrenumeChange}
+                            />
+                        </div>
+                        <CardFooter>
+                            <div className='flex flex-col w-full'>
+                                {error && (
+                                    <div className="text-red-500 text-sm text-center my-2">
+                                        {error}
+                                    </div>
+                                )}
+                                <Button
+                                    type="submit"
+                                    className="w-full"
+                                    disabled={loading}
+                                >
+                                    {loading ? (
+                                        <>
+                                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                            Please wait
+                                        </>
+                                    ) : (
+                                        'Sign in'
+                                    )}
+                                </Button>
+                            </div>
+                        </CardFooter>
+                    </form>
+                </CardContent>
+            </Card>
+        </>
     );
 }
