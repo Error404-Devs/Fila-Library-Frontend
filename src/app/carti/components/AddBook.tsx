2ba7d1192e1ae916ce0f17c6a70f85712ddf160a
David Rotariu <davidrotariu139@gmail.com>
2024-10-02 15:11:56 +0300

    Work on add books

--- src/app/carti/components/AddBook.tsx
+++ src/app/carti/components/AddBook.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import {
     Dialog,
@@ -9,8 +12,94 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useToast } from '@/components/ui/use-toast';
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import { useSession } from 'next-auth/react';
 
 const AddBook = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [quote, setQuote] = useState('');
+    const [year, setYear] = useState('');
+    const [location, setLocation] = useState('');
+    const [inventory, setInventory] = useState('');
+    const { toast } = useToast();
+    const { data: session } = useSession();
+    const accessToken = session?.access_token;
+
+    const isFormValid = () => {
+        return (
+            title.trim() !== '' &&
+            author.trim() !== '' &&
+            quote.trim() !== '' &&
+            year.trim() !== '' &&
+            location.trim() !== '' &&
+            inventory.trim() !== ''
+        );
+    };
+
+    const handleSubmit = async () => {
+        if (!isFormValid()) {
+            toast({
+                title: 'Error',
+                description: 'All fields are required!',
+                status: 'error'
+            });
+            return;
+        }
+
+        const bookData = {
+            title: title,
+            author: author,
+            quote: quote,
+            year: year,
+            location: location,
+            inventory: inventory
+        };
+
+        console.log(bookData);
+
+        try {
+            const response = await fetch(`${baseUrl}/books`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(bookData)
+            });
+
+            if (response.ok) {
+                toast({
+                    title: 'Success',
+                    description: `Book "${title}" has been added successfully!`,
+                    status: 'success'
+                });
+                setTitle('');
+                setAuthor('');
+                setQuote('');
+                setYear('');
+                setLocation('');
+                setInventory('');
+            } else {
+                console.error(`Error: ${response.statusText}`);
+                toast({
+                    title: 'Error',
+                    description: 'Failed to add the book!',
+                    status: 'error'
+                });
+            }
+        } catch (error) {
+            console.error('Error adding book:', error);
+            toast({
+                title: 'Error',
+                description: 'An unexpected error occurred.',
+                status: 'error'
+            });
+        }
+    };
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -24,44 +113,80 @@ const AddBook = () => {
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
+                        <Label htmlFor="title" className="text-right">
                             Titlu
                         </Label>
-                        <Input id="name" className="col-span-3" />
+                        <Input
+                            id="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
+                        <Label htmlFor="author" className="text-right">
                             Autor
                         </Label>
-                        <Input id="username" className="col-span-3" />
+                        <Input
+                            id="author"
+                            value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
+                        <Label htmlFor="quote" className="text-right">
                             Cota
                         </Label>
-                        <Input id="username" className="col-span-3" />
+                        <Input
+                            id="quote"
+                            value={quote}
+                            onChange={(e) => setQuote(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
+                        <Label htmlFor="year" className="text-right">
                             An Aparitie
                         </Label>
-                        <Input id="username" className="col-span-3" />
+                        <Input
+                            id="year"
+                            value={year}
+                            onChange={(e) => setYear(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
+                        <Label htmlFor="location" className="text-right">
                             Loc Aparitie
                         </Label>
-                        <Input id="username" className="col-span-3" />
+                        <Input
+                            id="location"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
+                        <Label htmlFor="inventory" className="text-right">
                             Inventar
                         </Label>
-                        <Input id="username" className="col-span-3" />
+                        <Input
+                            id="inventory"
+                            value={inventory}
+                            onChange={(e) => setInventory(e.target.value)}
+                            className="col-span-3"
+                        />
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button
+                        type="submit"
+                        onClick={handleSubmit}
+                        disabled={!isFormValid()}
+                    >
+                        Adauga Carte
+                    </Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
