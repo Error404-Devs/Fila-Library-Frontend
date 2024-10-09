380d3b5ec202b9c7e6dc61cb71ed7998ebb94325
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-10-09 10:59:31 +0300

    Adding wishlist button to search books

--- src/app/search/AvalaibeBooks.tsx
+++ src/app/search/AvalaibeBooks.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
     Table,
     TableBody,
@@ -7,8 +8,20 @@ import {
     TableRow
 } from '@/components/ui/table';
 import { BookType } from '../interfaces';
+import { Button } from '@/components/ui/button';
+import { Heart } from 'lucide-react';
+import { useState } from 'react';
 
 export default function AvailableBooks({ books }: { books: BookType[] }) {
+
+    const [favorites, setFavorites] = useState<boolean[]>(books.map(() => false));
+
+    const toggleFavorite = (index: number) => {
+        const updatedFavorites = [...favorites];
+        updatedFavorites[index] = !updatedFavorites[index];
+        setFavorites(updatedFavorites);
+    };
+
     return (
         <div>
             <Table className="bg-white dark:bg-gray-800">
@@ -16,14 +29,14 @@ export default function AvailableBooks({ books }: { books: BookType[] }) {
                     <TableRow>
                         <TableHead className="text-black dark:text-white">Titlu</TableHead>
                         <TableHead className="text-black dark:text-white">Autor</TableHead>
-                        <TableHead className="text-black dark:text-white">Categorie</TableHead>
+                        <TableHead className="text-black dark:text-white">Favorites</TableHead>
                         <TableHead className="text-black dark:text-white">An Aparitie</TableHead>
                         <TableHead className="text-black dark:text-white">Disponibile</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     {Array.isArray(books) &&
-                        books.map((book: any) => (
+                        books.map((book: BookType, index: number) => (
                             <TableRow
                                 key={book.id}
                                 className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-600"
@@ -35,7 +48,14 @@ export default function AvailableBooks({ books }: { books: BookType[] }) {
                                     {book.author}
                                 </TableCell>
                                 <TableCell className="p-[10px] text-black dark:text-white">
-                                    {book.category}
+                                    <Button
+                                        className='bg-transparent rounded-full hover:bg-transparent'
+                                        onClick={() => toggleFavorite(index)}
+                                    >
+                                        <Heart
+                                            className={favorites[index] ? 'text-red-400 fill-red-400' : 'text-black'}
+                                        />
+                                    </Button>
                                 </TableCell>
                                 <TableCell className="p-[10px] text-black dark:text-white">
                                     {book.year_of_publication || 'N/A'}
