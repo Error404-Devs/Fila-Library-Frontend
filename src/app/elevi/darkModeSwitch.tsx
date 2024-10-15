860e1de92f996c821e4dda542b34047d39cf948f
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-10-15 16:33:27 +0300

    Updating dark mode switch

--- src/app/elevi/darkModeSwitch.tsx
+++ src/app/elevi/darkModeSwitch.tsx
@@ -1,19 +1,18 @@
 import { useTheme } from "next-themes";
+import { Sun, Moon } from 'lucide-react';
+import "../../styles/styles.css"
 
 export const DarkModeSwitch = () => {
-
-    const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
-    <button
-      onClick={() => setTheme("dark")}
-      className="flex items-center justify-center w-12 h-6 bg-gray-200 dark:bg-gray-600 rounded-full p-1 transition-colors duration-300 focus:outline-none"
-    >
-      {/* <span
-        className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-           === 'dark' ? 'translate-x-6' : ''
-        }`}
-      /> */}
-    </button>
+    <>
+      <input type="checkbox" className="input" id="darkmode-toggle" checked={theme === "dark"}></input>
+      <label htmlFor="darkmode-toggle" className="button flex items-center justify-center w-12 h-6 bg-gray-200 dark:bg-gray-600 rounded-full p-1 transition-colors duration-300 focus:outline-none"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+        <span className="toggle-circle w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300"></span>
+      </label>
+    </>
   );
 };
